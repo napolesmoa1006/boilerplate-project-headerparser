1faec1c542ad72d9c2e45b0f1f8674382730c0aa
Carlos Napoles Avila <napolesmoa1006@gmail.com>
2023-09-14 18:53:31 -0400

    whoami feature

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  return res.json({
+    "ipaddress": req.ip,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on http://localhost:' + listener.address().port);
 });
